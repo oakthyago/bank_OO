40546fb1d2e19bdd53811cd488d5ddd45f3b907f
Thyago <thyagoborgescarvalho@gmail.com>
2026-06-24 21:49:04 -0300

    atribuição direta de  objetos entrelaçados

--- bank_oo-composto/src/testaBanco.java
+++ bank_oo-composto/src/testaBanco.java
@@ -1,14 +1,23 @@
 public class testaBanco {
     public static void main(String[] args) {
+
         Cliente oak = new Cliente();
-        oak.nome = "Oak Thyago";
+        oak.nome = "Thyago Carvalho";
         oak.cpf = "123.456.789-00";
         oak.profissao = "engenheiro";
 
         Conta contaDoThyago = new Conta();
         contaDoThyago.deposita(1621);
-        contaDoThyago.titular = oak;
+
+        contaDoThyago.titular = oak; //assossiação de objetos
+
         System.out.printf("O nome do cliente é : %s%n",contaDoThyago.titular.nome);
+
+        // atribuição direta
+        Conta contaDaVitoria = new Conta();
+        contaDaVitoria.titular =  new Cliente();
+        contaDaVitoria.titular.nome = "Vitoria Cristina";
+        System.out.printf("O nome do cliente com atribuição direta é : %s%n",contaDaVitoria.titular.nome);
 
 
 
